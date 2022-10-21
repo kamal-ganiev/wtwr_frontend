53f89fbf0057b58d3ec2402528d3242821855776
kamal-ganiev <kamal.ganiev999@gmail.com>
2022-10-20 23:16:22 -0400

    Made some changes with form.css and make text to be clickable when check checkbox

--- src/components/AddGarmentForm.js
+++ src/components/AddGarmentForm.js
@@ -34,129 +34,127 @@ function AddGarmentForm(props) {
   }
 
   return (
-    <>
-      <ModalWithForm
-        title="New garment"
-        id="AddingGarment"
-        buttonText="Add garment"
-        name="add"
-        isModalOpen={props.isModalOpen}
-        setIsModalOpen={props.setIsModalOpen}
-        handleEscClose={props.handleEscClose}
-        handleSubmit={(e) => {
-          handleSubmit(e);
-          props.setIsModalOpen(false);
-        }}
-      >
-        <label className="form__field modal__label">
-          <p className="form__input-title name-input">
-            Name <span className="form__error-message name-input-error"></span>
-          </p>
-          <input
-            className="form__input modal__field"
-            id="name-input"
-            type="text"
-            name="name"
-            placeholder="Name"
-            minLength="2"
-            maxLength="40"
-            onChange={(e) => {
-              setName(e.target.value);
-            }}
-            required
-          />
-        </label>
-        <label className="form__field modal__label">
-          <p className="form__input-title link-input">
-            Image <span className="form__error-message link-input-error"></span>
-          </p>
-          <input
-            className="form__input modal__field"
-            id="link-input"
-            name="link"
-            placeholder="Image URL"
-            type="url"
-            onChange={(e) => {
-              setImageUrl(e.target.value);
-            }}
-            required
-          />
-        </label>
-        <div className="modal__radio">
-          <h4 className="modal__radio-title radio__title">
-            Select the weather type:
-          </h4>
-          <ul className="form__radio-list">
-            <li className="form__radio-element" style={checked.hot}>
-              <label className="form__radio-label modal__label">
-                <input
-                  className="form__radio"
-                  type="radio"
-                  id="hot"
-                  value="hot"
-                  name="weather"
-                  onClick={(e) => {
-                    setWeather(e.target.value);
-                  }}
-                  onChange={() => {
-                    setHot(!hot);
-                    setCold(false);
-                    setWarm(false);
-                  }}
-                  required
-                />
-                <span className="form__checkmark" />
-              </label>
-              <p className="form__radio-text">Hot</p>
-            </li>
-            <li className="form__radio-element" style={checked.warm}>
-              <label className="form__radio-label modal__label">
-                <input
-                  className="form__radio"
-                  type="radio"
-                  id="warm"
-                  value="warm"
-                  name="weather"
-                  onClick={(e) => {
-                    setWeather(e.target.value);
-                  }}
-                  onChange={() => {
-                    setWarm(!warm);
-                    setCold(false);
-                    setHot(false);
-                  }}
-                  required
-                />
-                <span className="form__checkmark" />
-              </label>
-              <p className="form__radio-text">Warm</p>
-            </li>
-            <li className="form__radio-element" style={checked.cold}>
-              <label className="form__radio-label modal__label">
-                <input
-                  className="form__radio"
-                  type="radio"
-                  id="cold"
-                  value="cold"
-                  name="weather"
-                  onClick={(e) => {
-                    setWeather(e.target.value);
-                  }}
-                  onChange={() => {
-                    setCold(!cold);
-                    setWarm(false);
-                    setHot(false);
-                  }}
-                  required
-                />
-                <span className="form__checkmark" />
-              </label>
-              <p className="form__radio-text">Cold</p>
-            </li>
-          </ul>
-        </div>
-      </ModalWithForm>
-    </>
+    <ModalWithForm
+      title="New garment"
+      id="AddingGarment"
+      buttonText="Add garment"
+      name="add"
+      isModalOpen={props.isModalOpen}
+      setIsModalOpen={props.setIsModalOpen}
+      handleEscClose={props.handleEscClose}
+      handleSubmit={(e) => {
+        handleSubmit(e);
+        props.setIsModalOpen(false);
+      }}
+    >
+      <label className="form__field modal__label">
+        <p className="form__input-title name-input">
+          Name <span className="form__error-message name-input-error"></span>
+        </p>
+        <input
+          className="form__input modal__field"
+          id="name-input"
+          type="text"
+          name="name"
+          placeholder="Name"
+          minLength="2"
+          maxLength="40"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          required
+        />
+      </label>
+      <label className="form__field modal__label">
+        <p className="form__input-title link-input">
+          Image <span className="form__error-message link-input-error"></span>
+        </p>
+        <input
+          className="form__input modal__field"
+          id="link-input"
+          name="link"
+          placeholder="Image URL"
+          type="url"
+          onChange={(e) => {
+            setImageUrl(e.target.value);
+          }}
+          required
+        />
+      </label>
+      <div className="modal__radio">
+        <h4 className="modal__radio-title radio__title">
+          Select the weather type:
+        </h4>
+        <ul className="form__radio-list">
+          <li className="form__radio-element" style={checked.hot}>
+            <label className="form__radio-label modal__label">
+              <input
+                className="form__radio"
+                type="radio"
+                id="hot"
+                value="hot"
+                name="weather"
+                onClick={(e) => {
+                  setWeather(e.target.value);
+                }}
+                onChange={() => {
+                  setHot(!hot);
+                  setCold(false);
+                  setWarm(false);
+                }}
+                required
+              />
+              <span className="form__checkmark" />
+            </label>
+            <p className="form__radio-text">Hot</p>
+          </li>
+          <li className="form__radio-element" style={checked.warm}>
+            <label className="form__radio-label modal__label">
+              <input
+                className="form__radio"
+                type="radio"
+                id="warm"
+                value="warm"
+                name="weather"
+                onClick={(e) => {
+                  setWeather(e.target.value);
+                }}
+                onChange={() => {
+                  setWarm(!warm);
+                  setCold(false);
+                  setHot(false);
+                }}
+                required
+              />
+              <span className="form__checkmark" />
+            </label>
+            <p className="form__radio-text">Warm</p>
+          </li>
+          <li className="form__radio-element" style={checked.cold}>
+            <label className="form__radio-label modal__label">
+              <input
+                className="form__radio"
+                type="radio"
+                id="cold"
+                value="cold"
+                name="weather"
+                onClick={(e) => {
+                  setWeather(e.target.value);
+                }}
+                onChange={() => {
+                  setCold(!cold);
+                  setWarm(false);
+                  setHot(false);
+                }}
+                required
+              />
+              <span className="form__checkmark" />
+            </label>
+            <p className="form__radio-text">Cold</p>
+          </li>
+        </ul>
+      </div>
+    </ModalWithForm>
   );
 }
 
